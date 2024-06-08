238163cf8bba017c4726e7e117c0a6539247f0b9
Zayne <star1582@naver.com>
2024-06-08 16:47:22 +0100

    add Menu list

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,10 @@
 import { RxHamburgerMenu } from "react-icons/rx";
 import NikeLogo from "../assets/nike-logo.svg?react";
 
+const ROUTES = ["Home", "About", "Services", "Pricing", "Contact"];
 const Nav = () => {
   return (
-    <nav className="flex justify-between items-center">
+    <nav className="flex flex-wrap justify-between items-center">
       {/* Logo */}
       <a href="#">
         <NikeLogo className="h-20 w-20" />
@@ -12,6 +13,24 @@ const Nav = () => {
       <button className="hover:bg-gray-100 p-2 focus:ring-2 focus:ring-gray-200 rounded-lg">
         <RxHamburgerMenu size={25} />
       </button>
+
+      {/* Menu list */}
+      <div className="w-full">
+        <ul className="bg-gray-50 text-lg border border-gray-100 rounded-lg p-4">
+          {ROUTES.map((route, i) => {
+            return (
+              <li
+                className={`rounded py-2 px-3 cursor-pointer ${
+                  i === 0 ? "bg-blue-500 text-white" : "hover:bg-gray-100"
+                }`}
+                key={route}
+              >
+                {route}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </nav>
   );
 };
