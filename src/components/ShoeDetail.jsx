6c085ffab158213a4b3daae3ba3b6349d9656d0a
Zayne <star1582@naver.com>
2024-06-09 10:16:46 +0100

    Adding gradient and fonts

--- src/components/ShoeDetail.jsx
+++ src/components/ShoeDetail.jsx
@@ -3,10 +3,14 @@ import nike1 from "../assets/n1-min.png";
 const ShoeDetail = () => {
   return (
     <div className="flex flex-col lg:flex-row-reverse space-y-4">
+      {/* Shoe image */}
       <div className="flex-1">
-        <img src={nike1} />
+        <div className="bg-gradient-to-br from-[#F637CF] from-5% via-[#E3D876] via-50% to-[#4DD4C6]">
+          <img src={nike1} />
+        </div>
       </div>
       <div className="flex-1 space-y-6">
+        {/* Shoe details */}
         <div className="text-5xl font-black md:text-9xl">Nike Air max 270</div>
         <div className="font-medium md:text-xl">
           {
@@ -14,6 +18,8 @@ const ShoeDetail = () => {
           }
         </div>
         <div className="text-3xl font-extrabold md:text-6xl">100 $</div>
+
+        {/* Shoe Buttons and links */}
         <div className="space-x-10">
           <button className="h-14 w-44 bg-black text-white hover:bg-gray-900 active:bg-gray-700">
             Add to bag
